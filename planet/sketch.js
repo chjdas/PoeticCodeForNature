100d2f008c6d26bb8d49e6ce7b14bdf0488f8f7b
ChoiHyeJeoung <chjdas97@gmail.com>
2019-06-23 02:35:36 +0900

    sub_title

--- planet/sketch.js
+++ planet/sketch.js
@@ -8,7 +8,25 @@ let trails = false;
 
 function setup()
 {
-  createCanvas(300,300);
+//	createCanvas(300,300);
+	title = createElement('h3', "<a href='/PoeticCodeForNature'> HOME : </a> The sun, the planet");
+title.position (50,50);
+	canvas = createCanvas(300,300);
+	canvas.position(50,100);
+// 	description = "\
+// 	이 행성은 헷갈리던 부분의 거리상 구조는 맞지만 움직이는 둘 사이의 속력차로 이뤄지는 부분은 들어가지 않아 다른 아이디어로 떠올렸던 것이다.\
+//  제작에서 행성을 주로 하고 있기 때문인지 두 오브제의 속도차를 이용한 것으로 떠오른 예제다. \
+//  때문에 가상의 행성을 만들고 짜보는 코드로 다음과 같은 코드를 짜서 응용해보았다.<br/>\
+// 	 행성이 가장 중력이 큰 항성을 기준으로 타원을 그리며 돌게 된다.
+// 	 서로간의 질량차, 자전속도, 중력의 힘 등의 차이로 항성과 행성은 공전궤도에 올라 안정화 되는 과정을 거친다.
+// \
+// 	  ";
+// 	  text = createDiv(description);
+// 	  text.position(20, 400);
+// 	  text.style("font-family", "monospace");
+// 	  text.style("font-size", "12pt");
+
+
   background(0);
 
   x_sun = width/2;
